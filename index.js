3d5a935071154d0eaa3c1be91688d4e8940e04b2
Joel Hoil <yoelvann@gmail.com>
2022-04-27 17:13:57 -0500

    Refactoring - Finish to implement FizzbuzzSerive class with tests

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const Reader = require('./lib/utils/Reader');
 const ExplorerService = require('./lib/services/ExplorerService');
+const FizzbuzzService =require('./lib/services/FizzbuzzService');
 
 const explorers = Reader.readJsonFile("explorers.json");
 const explorersMission = ExplorerService.filterByMission(explorers, "node");
 const amountExplorersMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
 const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 
-// console.log(explorersMission);
-// console.log("-------------")
-// console.log(amountExplorersMission);
-console.log(explorersInNode)
+const explorer1 = {name: "Explorer1", score: 1}
+FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
--- tests/lib/services/FizzbuzzService.test.js
+++ tests/lib/services/FizzbuzzService.test.js
@@ -7,11 +7,52 @@ describe("Tests for FizzbuzzService class", () => {
     const jsonPath = path.join(__dirname, '..', '..', '..', 'explorers.json');
     const explorers = Reader.readJsonFile(jsonPath);
 
-    test('1. Test in applyValidationInExplorer(), need to add "trick" property with BUZZ value given an explorer with score = 3', () => { 
+    test('1. Test in applyValidationInExplorer(), need to add "trick" property given an explorer with score = n*3', () => { 
 
         const explorer1 = {name: "Explorer1", score: 3};
         const validateExplorer = FizzbuzzService.applyValidationInExplorer(explorer1);
 
         expect(validateExplorer).toHaveProperty('trick');
     });
+    
+    test('2. Test in applyValidationInExplorer(), need to add "trick" property with FIZZ value given an explorer with score = n*3', () => { 
+
+        const explorer1 = {name: "Explorer1", score: 6};
+        const validateExplorer = FizzbuzzService.applyValidationInExplorer(explorer1);
+
+        expect(validateExplorer.trick).toBe('FIZZ');
+    });
+
+    test('3. Test in applyValidationInExplorer(), need to add "trick" property given an explorer with score = n*5', () => { 
+
+        const explorer1 = {name: "Explorer1", score: 5};
+        const validateExplorer = FizzbuzzService.applyValidationInExplorer(explorer1);
+
+        expect(validateExplorer).toHaveProperty('trick');
+    });
+    
+    test('4. Test in applyValidationInExplorer(), need to add "trick" property with BUZZ value given an explorer with score = n*5', () => { 
+
+        const explorer1 = {name: "Explorer1", score: 10};
+        const validateExplorer = FizzbuzzService.applyValidationInExplorer(explorer1);
+
+        expect(validateExplorer.trick).toBe('BUZZ');
+    });
+
+    test('5. Test in applyValidationInExplorer(), need to add "trick" property with FIZZBUZZ value given an explorer with score divisible with n*3 and n*5', () => { 
+
+        const explorer1 = {name: "Explorer1", score: 15};
+        const validateExplorer = FizzbuzzService.applyValidationInExplorer(explorer1);
+
+        expect(validateExplorer.trick).toBe('FIZZBUZZ');
+    });
+
+    test('6. Test in applyValidationInExplorer(), need to add "trick" property with value equal to score value if score value can´t be divisible by 3, 5 or 3&5', () => { 
+
+        const explorer1 = {name: "Explorer1", score: 1};
+        const validateExplorer = FizzbuzzService.applyValidationInExplorer(explorer1);
+
+        expect(validateExplorer.trick).toBe(1);
+    });
+
 });
