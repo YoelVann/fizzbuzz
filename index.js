dc4499dbe9289b6087ba2723b23040dc7c2d0f84
Joel Hoil <yoelvann@gmail.com>
2022-04-27 00:17:03 -0500

    Refactoring - ExplorerService class implemented

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const Reader = require('./lib/utils/Reader');
+const ExplorerService = require('./lib/services/ExplorerService');
 
 const explorers = Reader.readJsonFile("explorers.json");
+const explorersMission = ExplorerService.filterByMission(explorers, "node");
+const amountExplorersMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+
+// console.log(explorersMission);
+// console.log("-------------")
+// console.log(amountExplorersMission);
+console.log(explorersInNode)
