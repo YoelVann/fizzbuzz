c26e532e06075a6067bc80be4ba86c34f38fcd7c
Joel Hoil <yoelvann@gmail.com>
2022-04-27 11:25:18 -0500

    tests for Reader class

---
+++ tests/lib/utils/Reader.test.js
@@ -0,0 +1,22 @@
+const Reader = require('./../../../lib/utils/Reader');
+const path = require('path');
+
+describe("Tests for the Reader class", () => {
+
+    test('1. readJsonFile() not be undefined', () => {
+
+        const jsonPath = path.join(__dirname, '..', '..', '..', 'explorers.json');
+        const explorers = Reader.readJsonFile(jsonPath);
+
+        expect(explorers).not.toBeUndefined();
+    });
+
+    test('2. readJsonFile() must be return an object', () => {
+
+        const jsonPath = path.join(__dirname, '..', '..', '..', 'explorers.json');
+        const explorers = Reader.readJsonFile(jsonPath);
+
+        console.log(explorers)
+        expect( typeof explorers).toBe('object');
+    });
+});
