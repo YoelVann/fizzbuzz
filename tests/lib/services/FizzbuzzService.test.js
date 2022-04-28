ff1eb866652443cae93495d45bc864a8324dc902
Joel Hoil <yoelvann@gmail.com>
2022-04-28 17:39:10 -0500

    Create a method fizzbuzz works on number

--- tests/lib/services/FizzbuzzService.test.js
+++ tests/lib/services/FizzbuzzService.test.js
@@ -1,11 +1,7 @@
 const FizzbuzzService = require("../../../lib/services/FizzbuzzService");
-const Reader = require("../../../lib/utils/Reader");
-const path = require("path");
 
 describe("Tests for FizzbuzzService class", () => { 
 
-    const jsonPath = path.join(__dirname, "..", "..", "..", "explorers.json");
-    const explorers = Reader.readJsonFile(jsonPath);
 
     test("1. Test in applyValidationInExplorer(), need to add \"trick\" property given an explorer with score = n*3", () => { 
 
@@ -53,6 +49,24 @@ describe("Tests for FizzbuzzService class", () => {
         const validateExplorer = FizzbuzzService.applyValidationInExplorer(explorer1);
 
         expect(validateExplorer.trick).toBe(1);
+    });
+
+    test("7. Test in applyValidationInNumber(), need to apply fizzbuzz validation given a number", () => { 
+
+        const number1 = 1;
+        const number2 = 3;
+        const number3 = 5;
+        const number4 = 15;
+
+        const validateNumber1 = FizzbuzzService.applyValidationInNumber(number1);
+        const validateNumber2 = FizzbuzzService.applyValidationInNumber(number2);
+        const validateNumber3 = FizzbuzzService.applyValidationInNumber(number3);
+        const validateNumber4 = FizzbuzzService.applyValidationInNumber(number4);
+
+        expect(validateNumber1).toBe(1);
+        expect(validateNumber2).toBe("FIZZ");
+        expect(validateNumber3).toBe("BUZZ");
+        expect(validateNumber4).toBe("FIZZBUZZ");
     });
 
 });
